2c9dbfc7128a318da50c9c55393edc86daf1ec48
Tony Seebregts <tony.seebregts@gmail.com>
2021-06-04 17:47:33 -0700

    Yet more Github workflow debug stuff

--- test/debug_spec.js
+++ test/debug_spec.js
@@ -1,15 +1,24 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
 const ip = require('ip')
+const os = require('os')
 
 describe('github', function () {
   describe('#detecting local IP configuration', function () {
     it('should detect IP address', function () {
       const addr = ip.address()
       const subnet = ip.subnet(addr, '255.255.255.0')
+      const interfaces = os.networkInterfaces()
 
       console.log(addr)
       console.log(subnet)
+      console.log(interfaces)
+
+      // for (const name of Object.keys(interfaces)) {
+      //   for (const net of interfaces[name]) {
+      //     console.log(net)
+      //   }
+      // }
     })
   })
 })
