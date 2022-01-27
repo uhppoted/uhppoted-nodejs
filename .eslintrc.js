0621ba8b84e44de254e97452d7e648d2f89103d7
Tony Seebregts <tony.seebregts@gmail.com>
2022-01-27 12:11:07 -0800

    Downgraded eslint to v7.32.0 for compatibility with eslint-config-standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true
   },
   extends: [
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
   rules: {
   }
